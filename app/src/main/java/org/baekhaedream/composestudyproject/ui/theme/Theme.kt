7cc590ef41abb35d51fa5bf2ea87759ba13f57ee
Park <pjh00098@gmail.com>
2022-01-22 17:25:32 +0900

    first commit

---
+++ app/src/main/java/org/baekhaedream/composestudyproject/ui/theme/Theme.kt
@@ -0,0 +1,54 @@
+package org.baekhaedream.composestudyproject.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Navy,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+    surface = Blue,
+    onSurface = Navy,
+    onPrimary = Chartreuse
+)
+
+private val LightColorPalette = lightColors(
+    primary = LightBlue,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+    surface = Blue,
+    onSurface = Color.White,
+    onPrimary = Navy
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun ComposeStudyProjectTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
